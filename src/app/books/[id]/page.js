090cc2e784f0c955c9a1f519c8f3e27ea3775132
Hillary Tanui <hillarytanui80@gmail.com>
2026-02-17 23:15:52 +0300

    Add dynamic book detail page route

---
+++ src/app/books/[id]/page.js
@@ -0,0 +1,21 @@
+import { fetchBookById } from '@/lib/api'
+import BookDetails from '@/components/books/BookDetails'
+
+export default async function BookPage() {
+  const { id } = await params
+  const book = await fetchBookById(id)
+
+  if (!book) {
+    return (
+      <div className="container mx-auto px-4 py-8">
+        <p className="text-center text-red-500">Book not found</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <BookDetails book={book} />
+    </div>
+  )
+}
