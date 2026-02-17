1543ce093d4f6f18d60ff718756a02566a348794
Hillary Tanui <hillarytanui80@gmail.com>
2026-02-17 21:35:54 +0300

    Add Footer component

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white mt-12">
+      <div className="container mx-auto px-4 py-6 text-center">
+        <p>&copy; 2026 Book Explorer. Data from Project Gutenberg.</p>
+      </div>
+    </footer>
+  )
+}
